3ac68292e9b9258f45e13389265e533cf08cd3fc
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2019-02-09 15:11:16 +0530

    release donavon's patch

--- dist.js
+++ dist.js
@@ -10,22 +10,23 @@ var _react = _interopRequireWildcard(require("react"));
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
 function useEventListener(eventName, callback) {
+  var element = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : window;
   var savedCallback = (0, _react.useRef)();
   (0, _react.useEffect)(function () {
     savedCallback.current = callback;
   }, [callback]);
   (0, _react.useEffect)(function () {
-    if (typeof window === 'undefined') return;
+    if (typeof element === 'undefined') return;
 
     var eventListener = function eventListener(event) {
       return savedCallback.current(event);
     };
 
-    window.addEventListener(eventName, eventListener);
+    element.addEventListener(eventName, eventListener);
     return function cleanup() {
-      return window.removeEventListener(eventName, eventListener);
+      return element.removeEventListener(eventName, eventListener);
     };
-  }, eventName);
+  }, [eventName]);
 }
 
 var _default = useEventListener;
